3a8a6899b069b87613cfa8ad141d4ffccc2d68cc
snir2808 <sniroos@gmail.com>
2020-10-22 16:14:51 +0300

    add

---
+++ script.js
@@ -0,0 +1,67 @@
+
+
+// video
+
+/* Get Our Elements */
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+/* Build out functions */
+function togglePlay(){
+   if(video.paused){
+      video.play();
+   } else{
+      video.pause();
+   };
+};
+function updateButton(){
+   if(this.paused){
+      toggle.textContent = '►'
+   }else{
+      toggle.textContent = '❚ ❚'
+   }
+}
+ function skip(){
+    video.currentTime += parseFloat(this.dataset.skip)
+ }
+
+ function handleRangeUpdate(){
+    video[this.name] = this.value
+ }
+ function handleProgress(){
+    const precent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${precent}%`;
+ }
+
+ function scrub(e){
+   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+   video.currentTime = scrubTime; 
+ }
+
+
+
+/* Hook up the event listeners */
+video.addEventListener('timeupdate', handleProgress );
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach( range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach( range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown =false
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove',(e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+// end video
+
